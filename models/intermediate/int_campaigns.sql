f7d69ab72ceee6c4766f263431aad272558a243f
Márcio Mundim <msmundim@gmail.com>
2024-06-14 01:27:31 +0000

    desafio completo

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,3 @@
+{{dbt_utils.union_relations(
+    relations=[ref("stg_raw__adwords"),ref("stg_raw__facebook"),ref("stg_raw__criteo"),ref("stg_raw__bing")]
+)}}
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -5,8 +5,10 @@ SELECT
     revenue, 
     quantity, 
     CAST(purchase_price AS FLOAT64) AS purchase_price, 
-    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+    ROUND(s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2),2) AS margin,
+    {{margin_percent("s.revenue","s.quantity * CAST(p.purchase_price AS FLOAT64)")}} as margin_percent
 FROM {{ref("stg_raw__sales")}} s
 LEFT JOIN {{ref("stg_raw__product")}} p 
     USING (products_id)
+
+
